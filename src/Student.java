e79bdf20cf42b93d4eced05a97706669916958c8
andre <Andrews.ru83@gmail.com>
2023-01-13 23:35:06 +0300

    Equals и hashCode

---
+++ src/Student.java
@@ -0,0 +1,28 @@
+import java.util.Objects;
+
+public class Student {
+
+    private String name;
+    private int age;
+
+    @Override
+    public boolean equals (Object o){
+        if (this == o){
+            return true;
+        }
+
+        if (o == null || getClass() != o.getClass()) {
+            return false;
+        }
+
+        Student student = new Student();
+        return age == student.age && Objects.equals(name, student.name);
+    }
+
+    @Override
+    public int hashCode(){
+        return Objects.hash(age, name);
+    }
+
+}
+
