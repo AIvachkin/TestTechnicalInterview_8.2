6d5a6482f51cbe297006d94b38e3366cfcea44b8
andre <Andrews.ru83@gmail.com>
2023-01-14 00:02:59 +0300

    Equals и hashCode

--- src/Main.java
+++ src/Main.java
@@ -2,7 +2,9 @@ public class Main {
 
     public static void main(String[] args) {
 
-
+//  - Задача
+//
+//   Напишите методы Equals and HashCode для класса Student, который состоит из полей String name и int age
 
     }
 }
